51ae89c004b6fddd16df684292ac2eaa3a5b9f5b
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-06-13 15:46:45 +0700

    fix

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
-  mount_uploader :picture, PictureUploader
   default_scope -> { order(created_at: :desc) }
   validates :body, presence: true, length: { maximum: 140 }
   validates :title, presence: true, length: {minimum: 1, maximum: 20}
